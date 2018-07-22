d4f0e362a4fae89e53dddda7c97a3aa0fb54d2c0
Przemyslaw Jablonski <sharaquss@gmail.com>
2018-07-23 01:12:07 +0200

    initial

---
+++ wypok/Frontpage/WypokFrontPageView.swift
@@ -0,0 +1,13 @@
+//
+//  FrontPageView.swift
+//  wypok
+//
+//  Created by Przemyslaw Jablonski on 23/07/2018.
+//  Copyright © 2018 Przemyslaw Jablonski. All rights reserved.
+//
+
+import Foundation
+
+protocol WypokFrontPageView {
+    func render(with viewState: WypokFrontPageViewState)
+}
---
+++ wypok/Frontpage/WypokFrontPageViewState.swift
@@ -0,0 +1,15 @@
+//
+//  FrontPageViewState.swift
+//  wypok
+//
+//  Created by Przemyslaw Jablonski on 23/07/2018.
+//  Copyright © 2018 Przemyslaw Jablonski. All rights reserved.
+//
+
+import Foundation
+
+enum WypokFrontPageViewState {
+    case ERROR
+    case LOADING
+    case ARTICLES_LIST
+}
